dcade51a20e0edf8a02084dd311ee218fb924087
Andriikryl <andreyk557@gmail.com>
2024-05-28 18:55:39 +0300

    add useInterval custom hook

---
+++ src/hooks/useIterval.tsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect, useRef } from "react";
+
+export function useInterval(callback: any, delay: any) {
+  const savedCallback = useRef<any>();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      // Check if savedCallback.current is defined before calling it
+      if (savedCallback.current) {
+        savedCallback.current();
+      }
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
